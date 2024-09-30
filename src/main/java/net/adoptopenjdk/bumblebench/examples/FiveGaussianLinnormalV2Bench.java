f7b0a7d2af6feb8a845011c964ecda8956641c00
Tommy Ettinger <tommy.ettinger@gmail.com>
2024-09-29 21:40:19 -0700

    If normalV2() replaces normal()...
    ... Then it would still be the fastest version we have. This one curves a lot more in the trail than the current version, which appears to be a line in the trail, and a steeper one than it should be...

---
+++ src/main/java/net/adoptopenjdk/bumblebench/examples/FiveGaussianLinnormalV2Bench.java
@@ -0,0 +1,128 @@
+/*******************************************************************************
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
+package net.adoptopenjdk.bumblebench.examples;
+
+import com.github.tommyettinger.random.GoldenQuasiRandom;
+import net.adoptopenjdk.bumblebench.core.MicroBench;
+
+/**
+ * Windows 11, 12th Gen i7-12800H at 2.40 GHz:
+ * <br>
+ * HotSpot Java 8 (BellSoft):
+ * <br>
+ * FiveGaussianLinnormalV2Bench score: 65466664.000000 (65.47M 1799.7%)
+ *                          uncertainty:   0.2%
+ * <br>
+ * HotSpot Java 17 (Adoptium):
+ * <br>
+ * FiveGaussianLinnormalV2Bench score: 107739728.000000 (107.7M 1849.5%)
+ *                          uncertainty:   0.2%
+ * <br>
+ * HotSpot Java 21 (BellSoft):
+ * <br>
+ * FiveGaussianLinnormalV2Bench score: 102382656.000000 (102.4M 1844.4%)
+ *                          uncertainty:   0.3%
+ * <br>
+ * GraalVM Java 22:
+ * <br>
+ * FiveGaussianLinnormalV2Bench score: 131421896.000000 (131.4M 1869.4%)
+ *                          uncertainty:   1.0%
+ */
+public final class FiveGaussianLinnormalV2Bench extends MicroBench {
+
+	protected long doBatch(long numIterations) throws InterruptedException {
+		GoldenQuasiRandom rng = new GoldenQuasiRandom(0x12345678L);
+		double sum = 0.0;
+		for (long i = 0; i < numIterations; i++) {
+			sum += Linnormal.normalV2(rng.nextLong());
+			sum += Linnormal.normalV2(rng.nextLong());
+			sum += Linnormal.normalV2(rng.nextLong());
+			sum += Linnormal.normalV2(rng.nextLong());
+			sum += Linnormal.normalV2(rng.nextLong());
+        }
+		return numIterations;
+	}
+}
+
+// TEMPLATE
+
+/* *
+ * Windows 11, 12th Gen i7-12800H at 2.40 GHz:
+ * <br>
+ * HotSpot Java 8 (BellSoft):
+ * <br>
+ *
+ * <br>
+ * HotSpot Java 17 (Adoptium):
+ * <br>
+ *
+ * <br>
+ * HotSpot Java 21 (BellSoft):
+ * <br>
+ *
+ * <br>
+ * GraalVM Java 22:
+ * <br>
+ *
+ */
+// OLD, uses PouchRandom
+/* *
+ * Windows 11, 12th Gen i7-12800H at 2.40 GHz:
+ * <br>
+ * HotSpot Java 8 (BellSoft):
+ * <br>
+ * FiveGaussianLinnormalBench score: 107790920.000000 (107.8M 1849.6%)
+ *                        uncertainty:   0.5%
+ * <br>
+ * HotSpot Java 17 (Adoptium):
+ * <br>
+ * FiveGaussianLinnormalBench score: 58685964.000000 (58.69M 1788.8%)
+ *                        uncertainty:   0.4%
+ * <br>
+ * HotSpot Java 21 (BellSoft):
+ * <br>
+ * FiveGaussianLinnormalBench score: 40428100.000000 (40.43M 1751.5%)
+ *                        uncertainty:   0.4%
+ * <br>
+ * GraalVM Java 22:
+ * <br>
+ * FiveGaussianLinnormalBench score: 103583752.000000 (103.6M 1845.6%)
+ *                        uncertainty:   0.9%
+ */
+
+// OLD, has problems in the trail of the distribution
+/*
+ * Windows 11, 12th Gen i7-12800H at 2.40 GHz:
+ * <br>
+ * HotSpot Java 8 (BellSoft):
+ * <br>
+ * FiveGaussianLinnormalBench score: 88797264.000000 (88.80M 1830.2%)
+ *                      uncertainty:   0.4%
+ * <br>
+ * HotSpot Java 17 (Adoptium):
+ * <br>
+ * FiveGaussianLinnormalBench score: 87081856.000000 (87.08M 1828.2%)
+ *                      uncertainty:   0.4%
+ * <br>
+ * HotSpot Java 21 (BellSoft):
+ * <br>
+ * FiveGaussianLinnormalBench score: 103184184.000000 (103.2M 1845.2%)
+ *                      uncertainty:   1.0%
+ * <br>
+ * GraalVM Java 22:
+ * <br>
+ * FiveGaussianLinnormalBench score: 106505464.000000 (106.5M 1848.4%)
+ *                      uncertainty:   0.8
+ */
