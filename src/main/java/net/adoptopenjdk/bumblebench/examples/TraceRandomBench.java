72c6ea76dc1988072f66259b0947a1749ec85ceb
Tommy Ettinger <tommy.ettinger@gmail.com>
2025-09-07 22:31:24 -0700

    Updates all over; Mace to Trace change.

--- src/main/java/net/adoptopenjdk/bumblebench/examples/TraceRandomBench.java
+++ src/main/java/net/adoptopenjdk/bumblebench/examples/TraceRandomBench.java
@@ -15,10 +15,43 @@
 
 package net.adoptopenjdk.bumblebench.examples;
 
-import com.github.tommyettinger.random.MaceRandom;
+import com.github.tommyettinger.random.TraceRandom;
 import net.adoptopenjdk.bumblebench.core.MicroBench;
 
 /**
+ * Windows 11, 12th Gen i7-12800H at 2.40 GHz:
+ * <br>
+ * HotSpot Java 8 (BellSoft):
+ * <br>
+ * TraceRandomBench score: 1085878528.000000 (1.086G 2080.6%)
+ *              uncertainty:  22.6%
+ * <br>
+ * HotSpot Java 17 (Adoptium):
+ * <br>
+ * TraceRandomBench score: 2496134656.000000 (2.496G 2163.8%)
+ *              uncertainty:   0.3%
+ * <br>
+ * HotSpot Java 21 (BellSoft):
+ * <br>
+ * TraceRandomBench score: 2462381824.000000 (2.462G 2162.4%)
+ *              uncertainty:   0.6
+ * <br>
+ * GraalVM Java 24:
+ * <br>
+ * TraceRandomBench score: 2510592512.000000 (2.511G 2164.4%)
+ *              uncertainty:   0.4%
+ */
+public final class TraceRandomBench extends MicroBench {
+
+	private long sum = 0L;
+	protected long doBatch(long numIterations) throws InterruptedException {
+		TraceRandom rng = new TraceRandom(0x12345678);
+		for (long i = 0; i < numIterations; i++)
+			sum += rng.nextLong();
+		return numIterations;
+	}
+}
+/* *
  * Windows 11, 12th Gen i7-12800H at 2.40 GHz:
  * <br>
  * HotSpot Java 8 (BellSoft):
@@ -41,27 +74,3 @@ import net.adoptopenjdk.bumblebench.core.MicroBench;
  * MaceRandomBench score: 2759608064.000000 (2.760G 2173.8%)
  *             uncertainty:   0.3%
  */
-public final class MaceRandomBench extends MicroBench {
-
-	private long sum = 0L;
-	protected long doBatch(long numIterations) throws InterruptedException {
-		MaceRandom rng = new MaceRandom(0x12345678);
-		for (long i = 0; i < numIterations; i++)
-			sum += rng.nextLong();
-		return numIterations;
-	}
-
-	/**
-	 * Optionally implemented by subclasses and called at the end of a run to verify
-	 * whether the run was correct or not. Defaults to true. If false, an ERROR message
-	 * is printed instead of the final score. Implementing methods may output their own
-	 * error message(s) as well.
-	 */
-	@Override
-	protected boolean verify() {
-		System.out.println("Final sum was: ");
-		System.out.println(sum);
-		System.out.println();
-		return super.verify();
-	}
-}
